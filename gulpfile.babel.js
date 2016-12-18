7c39d04b32ebcbcffd590a42827ec507d37d0558
Lee Allen <lee@leeallen.net>
2016-12-18 11:58:50 -0600

    Added deployment tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,52 +3,58 @@ import browserSync from 'browser-sync';
 import cssnano from 'cssnano';
 import del from 'del';
 import gulp from 'gulp';
+import gzip from 'gulp-gzip';
 import htmlmin from 'gulp-htmlmin';
 import imagemin from 'gulp-imagemin';
 import os from 'os';
 import parallel from 'concurrent-transform';
 import postcss from 'gulp-postcss';
-import pump from 'pump';
 import runSequence from 'run-sequence';
 import sass from 'gulp-sass';
+import ssh from 'gulp-ssh';
+import { sshConfig, sshPath } from './sshConfig';
 import sourcemaps from 'gulp-sourcemaps';
+import tar from 'gulp-tar';
 import uglify from 'gulp-uglify';
 
 const cores = os.cpus().length;
 const browserSyncInstance = browserSync.create();
 const paths = {
   build: 'build',
+  remote: sshPath,
   source: 'source',
 };
+const shellConfig = {
+  ignoreErrors: false,
+  sshConfig: sshConfig,
+};
+const shellInstance = ssh(shellConfig);
 
-gulp.task('build', () => {
-  runSequence(
+gulp.task('build', (done) => {
+  return runSequence(
     'build:clean',
     'build:copy',
     ['build:html', 'build:images', 'build:styles', 'build:scripts'],
+    done,
   );
 });
 
-gulp.task('build:clean', () => {
-  return del(`${paths.build}/**/*`, {dot: true});
-});
+gulp.task('build:clean', () => del(`${paths.build}/**/*`, {dot: true}));
 
 gulp.task('build:copy', () => {
-  pump([
-    gulp.src([
-      `${paths.source}/.*`,
-      `${paths.source}/*.*`,
-      `${paths.source}/**/*`,
-      `!${paths.source}/*.html`,
-      `!${paths.source}/images`,
-      `!${paths.source}/images/**/*`,
-      `!${paths.source}/styles`,
-      `!${paths.source}/styles/**/*`,
-      `!${paths.source}/scripts`,
-      `!${paths.source}/scripts/**/*`,
-    ]),
-    gulp.dest(paths.build),
-  ], () => {});
+  return gulp.src([
+    `${paths.source}/.*`,
+    `${paths.source}/*.*`,
+    `${paths.source}/**/*`,
+    `!${paths.source}/*.html`,
+    `!${paths.source}/images`,
+    `!${paths.source}/images/**/*`,
+    `!${paths.source}/styles`,
+    `!${paths.source}/styles/**/*`,
+    `!${paths.source}/scripts`,
+    `!${paths.source}/scripts/**/*`,
+  ])
+    .pipe(gulp.dest(paths.build));
 });
 
 gulp.task('build:html', () => {
@@ -64,28 +70,22 @@ gulp.task('build:html', () => {
     removeStyleLinkTypeAttributes: true,
   };
 
-  pump([
-    gulp.src(`${paths.source}/*.html`),
-    htmlmin(config),
-    gulp.dest(paths.build),
-  ], () => {});
+  return gulp.src(`${paths.source}/*.html`)
+    .pipe(htmlmin(config))
+    .pipe(gulp.dest(paths.build));
 });
 
 gulp.task('build:images', () => {
   // TODO: Add more fine-grained image optimization including WebP support
-  pump([
-    gulp.src(`${paths.source}/images/**/*.{png,jpg,jpeg,gif,svg}`),
-    parallel(imagemin(), cores),
-    gulp.dest(`${paths.build}/images`),
-  ], () => {});
+  return gulp.src(`${paths.source}/images/**/*.{png,jpg,jpeg,gif,svg}`)
+    .pipe(parallel(imagemin(), cores))
+    .pipe(gulp.dest(`${paths.build}/images`));
 });
 
 gulp.task('build:scripts', () => {
-  pump([
-    gulp.src(`${paths.source}/scripts/**/*`),
-    uglify(),
-    gulp.dest(`${paths.build}/scripts`),
-  ], () => {});
+  return gulp.src(`${paths.source}/scripts/**/*.js`)
+    .pipe(uglify())
+    .pipe(gulp.dest(`${paths.build}/scripts`));
 });
 
 gulp.task('build:styles', () => {
@@ -94,12 +94,60 @@ gulp.task('build:styles', () => {
     cssnano(),
   ];
 
-  pump([
-    gulp.src(`${paths.source}/styles/main.scss`),
-    sass.sync().on('error', sass.logError),
-    postcss(processors),
-    gulp.dest(`${paths.build}/styles`),
-  ], () => {});
+  return gulp.src(`${paths.source}/styles/main.scss`)
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(`${paths.build}/styles`));
+});
+
+// This task will create a new distribution folder and deploy it to the server.
+gulp.task('deploy', ['build'], (done) => {
+  return runSequence(
+    'deploy:clean',
+    'deploy:compress',
+    'deploy:clean-remote',
+    'deploy:upload',
+    'deploy:extract',
+    done,
+  );
+});
+
+// This deployment helper task deletes the current distribution tarball, if one exists.
+gulp.task('deploy:clean', () => del('build.tar.gz'));
+
+// This deployment helper task creates a tarball from the distribution folder.
+gulp.task('deploy:compress', () => {
+  return gulp.src([
+    `${paths.build}/**/*`,
+    `${paths.build}/.htaccess`,
+  ])
+    .pipe(tar('build.tar'))
+    .pipe(gzip())
+    .pipe(gulp.dest('.'));
+});
+
+// This deployment helper task deletes all existing files in the server directory.
+gulp.task('deploy:clean-remote', () => {
+  return shellInstance.shell([
+    `cd ${paths.remote}`,
+    'rm -rf ./*',
+    'rm -rf ./.htaccess',
+  ], {filePath: 'clean-remote-commands.log'})
+    .pipe(gulp.dest('sshLogs'));
+});
+
+// This deployment helper task uploads the distribution tarball to the server directory.
+gulp.task('deploy:upload', () => gulp.src('build.tar.gz').pipe(shellInstance.sftp('write', `${paths.remote}build.tar.gz`)));
+
+// This deployment helper task extracts the distribution tarball to the server directory and then deletes the tarball.
+gulp.task('deploy:extract', () => {
+  return shellInstance.shell([
+    `cd ${paths.remote}`,
+    'gunzip ./build.tar.gz',
+    'tar -xvf ./build.tar --overwrite',
+    'rm ./build.tar'
+  ], {filePath: 'extract-commands.log'})
+    .pipe(gulp.dest('sshLogs'));
 });
 
 gulp.task('css', () => {
