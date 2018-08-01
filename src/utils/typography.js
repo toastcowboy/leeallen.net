67b828a56f5c4a49e1b47ea719e0df2ff5edb1a8
Lee Allen <lee@leeallen.net>
2018-08-01 09:15:05 -0700

    Add utility header classes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -8,39 +8,39 @@ const fontStack = [
   'sans-serif',
 ];
 
-export default new Typography(
-  {
-    baseFontSize: '16px',
-    baseLineHeight: 1.5,
-    scaleRatio: 1.5,
-    googleFonts: [
-      {
-        name: 'Raleway',
-        styles: [
-          '400',
-          '600',
-        ],
-      },
-    ],
-    headerFontFamily: fontStack,
-    bodyFontFamily: fontStack,
-    headerColor: 'inherit',
-    bodyColor: 'hsl(0, 0%, 0%, 0.8)',
-    headerWeight: 600,
-    bodyWeight: 400,
-    boldWeight: 600,
-    blockMarginBottom: '24px',
-    includeNormalize: true,
-    overrideStyles: ({adjustFontSizeTo, rhythm}, options, styles) => ({
-      '.typography-regular': {
-        fontWeight: 400,
-      },
-      '.typography-semi-bold': {
-        fontWeight: 600,
-      },
-      '.typography-small': {
-        ...adjustFontSizeTo('12px'),
-      },
-    }),
-  },
-);
+export default new Typography({
+  baseFontSize: '16px',
+  baseLineHeight: 1.5,
+  scaleRatio: 1.5,
+  googleFonts: [
+    {
+      name: 'Raleway',
+      styles: [
+        '400',
+        '600',
+      ],
+    },
+  ],
+  headerFontFamily: fontStack,
+  bodyFontFamily: fontStack,
+  headerColor: 'inherit',
+  bodyColor: 'hsl(0, 0%, 0%, 0.8)',
+  headerWeight: 600,
+  bodyWeight: 400,
+  boldWeight: 600,
+  blockMarginBottom: '24px',
+  includeNormalize: true,
+  overrideStyles: ({adjustFontSizeTo, rhythm}, options, styles) => ({
+    '.typography-h1': styles.h1,
+    '.typography-h2': styles.h2,
+    '.typography-h3': styles.h3,
+    '.typography-h4': styles.h4,
+    '.typography-h5': styles.h5,
+    '.typography-h6': styles.h6,
+    '.typography-regular': options.bodyWeight,
+    '.typography-semi-bold': options.boldWeight,
+    '.typography-small': {
+      ...adjustFontSizeTo('12px'),
+    },
+  }),
+});
