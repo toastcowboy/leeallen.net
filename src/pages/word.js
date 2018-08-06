9f434e8f8c458fb4d33023f52d7a6f86a39058a0
Lee Allen <lee@leeallen.net>
2018-08-06 10:27:45 -0700

    Add post page

--- src/pages/word.js
+++ src/pages/word.js
@@ -10,19 +10,19 @@ export default () => (
       date="July 4th, 2018"
       excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nunc tincidunt, dictum risus vitae, interdum augue. Sed mollis arcu feugiat, egestas purus quis, gravida tortor. Sed consequat felis sit amet ligula tempor dapibus id vel lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nunc tincidunt, dictum risus vitae, interdum augue. Sed mollis arcu feugiat, egestas purus quis, gravida tortor. Sed consequat felis sit amet ligula tempor dapibus id vel lacus."
       image={{ altText: 'Post image', src: 'https://source.unsplash.com/random/1370x771' }}
-      link="#"
+      link="/post"
       title="Sweet blog post title that goes onto two lines"/>
     <Post
       date="June 23rd, 2018"
       excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nunc tincidunt, dictum risus vitae, interdum augue. Sed mollis arcu feugiat, egestas purus quis, gravida tortor. Sed consequat felis sit amet ligula tempor dapibus id vel lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nunc tincidunt, dictum risus vitae, interdum augue. Sed mollis arcu feugiat, egestas purus quis, gravida tortor. Sed consequat felis sit amet ligula tempor dapibus id vel lacus."
       image={{ altText: 'Post image', src: 'https://source.unsplash.com/random/1370x771' }}
-      link="#"
+      link="/post"
       title="This is another post title that just breaks"/>
     <Post
       date="May 4th, 2018"
       excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nunc tincidunt, dictum risus vitae, interdum augue. Sed mollis arcu feugiat, egestas purus quis, gravida tortor. Sed consequat felis sit amet ligula tempor dapibus id vel lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in nunc tincidunt, dictum risus vitae, interdum augue. Sed mollis arcu feugiat, egestas purus quis, gravida tortor. Sed consequat felis sit amet ligula tempor dapibus id vel lacus."
       image={{ altText: 'Post image', src: 'https://source.unsplash.com/random/1370x771' }}
-      link="#"
+      link="/post"
       title="Yet another terrible title that goes on"/>
   </div>
 );
