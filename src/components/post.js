9c5dbe892db28303dffa5901c39579060d338b57
Lee Allen <lee@leeallen.net>
2018-08-09 08:36:16 -0700

    Fix improper use of anchor tags with Link component and link post images to posts

--- src/components/post.js
+++ src/components/post.js
@@ -1,3 +1,4 @@
+import Link from 'gatsby-link';
 import React from 'react';
 
 import styles from './post.module.css';
@@ -7,12 +8,16 @@ import Shiitake from 'shiitake';
 export default props => (
   <div className={[styles.post, 'typography-line-break-half'].join(' ')}>
     <h2 className={[styles.postTitle, 'typography-h1'].join(' ')}>{props.title}</h2>
-    <p className="typography-line-break-half typography-small"><a href={props.link}>{props.date}</a></p>
-    <img
-      className={styles.postImage}
-      src={props.image.src}
-      width={1370}
-      alt={props.image.altText}/>
+    <p className="typography-line-break-half typography-small">
+      <Link to={props.link}>{props.date}</Link>
+    </p>
+    <Link to={props.link}>
+      <img
+        className={styles.postImage}
+        src={props.image.src}
+        width={1370}
+        alt={props.image.altText}/>
+    </Link>
     {props.excerpt ? (
       <Shiitake
         className="typography-line-break-none"
@@ -26,7 +31,7 @@ export default props => (
       props.body.map((body, index) => <p key={index}>{body}</p> )
     ) : null}
     {props.excerpt ? (
-      <p className="typography-small"><a href={props.link}>Read more</a></p>
+      <p className="typography-small"><Link to={props.link}>Read more</Link></p>
     ) : null}
   </div>
 );
