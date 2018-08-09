097464bfcd8d3359b9792af56b6f279e1d750c3b
Lee Allen <lee@leeallen.net>
2018-08-09 08:51:22 -0700

    Only link post images when there's an exerpt

--- src/components/post.js
+++ src/components/post.js
@@ -5,19 +5,35 @@ import styles from './post.module.css';
 
 import Shiitake from 'shiitake';
 
+const HeroImage = props => (
+  <img
+    className={props.className}
+    src={props.src}
+    width={1370}
+    alt={props.alt}/>
+);
+
 export default props => (
   <div className={[styles.post, 'typography-line-break-half'].join(' ')}>
     <h2 className={[styles.postTitle, 'typography-h1'].join(' ')}>{props.title}</h2>
     <p className="typography-line-break-half typography-small">
       <Link to={props.link}>{props.date}</Link>
     </p>
-    <Link to={props.link}>
-      <img
+    {props.excerpt ? (
+      <Link to={props.link}>
+        <HeroImage
+          className={styles.postImage}
+          src={props.image.src}
+          width={1370}
+          alt={props.image.altText}/>
+      </Link>
+    ) : (
+      <HeroImage
         className={styles.postImage}
         src={props.image.src}
         width={1370}
         alt={props.image.altText}/>
-    </Link>
+    )}
     {props.excerpt ? (
       <Shiitake
         className="typography-line-break-none"
