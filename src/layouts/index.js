c189770fce5f105e630e82654ecf2ff723e32a6d
Lee Allen <lee@leeallen.net>
2018-08-13 09:57:07 -0700

    Workaround mobile sizing issues to make sure footer stays pinned to the bottom of the viewport

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Footer from '../components/footer';
 import Header from '../components/header';
 
 import styles from './index.module.css';
 
-export default props => (
-  <div className={styles.container}>
-    <div/>
-    <div className={styles.content}>
-      <Header pathname={props.location.pathname}/>
-      <main>{props.children()}</main>
-      <Footer/>
-    </div>
-    <div/>
-  </div>
-);
+export default class extends Component {
+  constructor(props) {
+    super(props);
+
+    // Create a callback ref for the content element
+    this.content = null;
+    this.setContentRef = element => { this.content = element; };
+
+    // Bind event handlers
+    this.handleResize = this.handleResize.bind(this);
+
+    /**
+     * Set the minHeight dynamically to workaround mobile sizing issues and keep the footer pinned
+     * to the bottom of the viewport
+     */
+    this.setContentHeight = () => {
+      if (this.content) this.content.style.minHeight = `${window.innerHeight}px`;
+    };
+  }
+
+  componentDidMount() {
+    this.setContentHeight();
+
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize(event) {
+    this.setContentHeight();
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div/>
+        <div className={styles.content} ref={this.setContentRef}>
+          <Header pathname={this.props.location.pathname}/>
+          <main>{this.props.children()}</main>
+          <Footer/>
+        </div>
+        <div/>
+      </div>
+    );
+  }
+}
