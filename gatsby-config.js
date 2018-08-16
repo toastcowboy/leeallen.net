d54016a7b1814f1365419c27817c929cc1b273e9
Lee Allen <lee@leeallen.net>
2018-08-16 09:21:09 -0700

    Properly handle images in Markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,9 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-sharp',
+    },
+    {
       resolve: 'gatsby-plugin-sitemap',
     },
     {
@@ -51,6 +54,19 @@ module.exports = {
     },
     {
       resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 685,
+            },
+          },
+        ],
+      },
     },
   ],
 };
