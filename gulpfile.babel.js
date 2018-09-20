95c2acbf08416185eeeb43b3ab810c13a79604bf
Lee Allen <lee@leeallen.net>
2018-09-20 09:32:43 -0700

    Add first three steps of deployment process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,24 @@
 import del from 'del';
 import gulp from 'gulp';
-// import gzip from 'gulp-gzip';
+import GulpSSH from 'gulp-ssh';
+import gzip from 'gulp-gzip';
 // import runSequence from 'run-sequence';
-// import ssh from 'gulp-ssh';
-// import { sshConfig, sshPath } from './sshConfig';
-// import tar from 'gulp-tar';
+import { shellConfig, sshPath } from './sshConfig';
+import tar from 'gulp-tar';
 
-// const paths = {
-//   build: 'build',
-//   remote: sshPath,
-//   source: 'source',
-// };
+const paths = {
+  build: `public`,
+  remote: sshPath,
+  source: `src`,
+};
 // const shellConfig = {
 //   ignoreErrors: false,
 //   sshConfig: sshConfig,
 // };
-// const shellInstance = ssh(shellConfig);
+const shellInstance = new GulpSSH({
+  ignoreErrors: false,
+  sshConfig: shellConfig,
+});
 
 // This task will create a new distribution folder and deploy it to the server.
 // gulp.task('deploy', (done) => {
@@ -30,28 +33,28 @@ import gulp from 'gulp';
 // });
 
 // This deployment helper task deletes the current distribution tarball, if one exists.
-gulp.task('deploy:clean', () => del('public.tar.gz'));
+gulp.task(`deploy:clean`, () => del(`public.tar.gz`));
 
 // This deployment helper task creates a tarball from the distribution folder.
-// gulp.task('deploy:compress', () => {
-//   return gulp.src([
-//     `${paths.build}/**/*`,
-//     `${paths.build}/.htaccess`,
-//   ])
-//     .pipe(tar('build.tar'))
-//     .pipe(gzip())
-//     .pipe(gulp.dest('.'));
-// });
+gulp.task(`deploy:compress`, () => {
+  return gulp.src([
+    `${paths.build}/**/*`,
+    `${paths.build}/.htaccess`,
+  ])
+    .pipe(tar(`public.tar`))
+    .pipe(gzip())
+    .pipe(gulp.dest(`.`));
+});
 
 // This deployment helper task deletes all existing files in the server directory.
-// gulp.task('deploy:clean-remote', () => {
-//   return shellInstance.shell([
-//     `cd ${paths.remote}`,
-//     'rm -rf ./*',
-//     'rm -rf ./.htaccess',
-//   ], {filePath: 'clean-remote-commands.log'})
-//     .pipe(gulp.dest('sshLogs'));
-// });
+gulp.task(`deploy:clean-remote`, () => {
+  return shellInstance.shell([
+    `cd ${paths.remote}`,
+    `rm -rf ./*`,
+    `rm -rf ./.htaccess`,
+  ], {filePath: `clean-remote-commands.log`})
+    .pipe(gulp.dest(`sshLogs`));
+});
 
 // This deployment helper task uploads the distribution tarball to the server directory.
 // gulp.task('deploy:upload', () => gulp.src('build.tar.gz').pipe(shellInstance.sftp('write', `${paths.remote}build.tar.gz`)));
